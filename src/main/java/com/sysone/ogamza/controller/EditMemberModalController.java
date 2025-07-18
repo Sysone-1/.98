529cad8f60fd5cc36ecb6cc1bbf61e6b276cd3a6
HeHelee <selly3109@naver.com>
2025-07-18 09:20:36 +0900

    feat: 회원 수정 모달 컨트롤러 설정

---
+++ src/main/java/com/sysone/ogamza/controller/EditMemberModalController.java
@@ -0,0 +1,11 @@
+package com.sysone.ogamza.controller;
+
+import javafx.event.ActionEvent;
+
+public class EditMemberModalController {
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
+}
