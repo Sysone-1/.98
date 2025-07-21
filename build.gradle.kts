1a9d958708b272258ff590ffcfdb22e669387767
YoonYn9915 <jsy440720@gmail.com>
2025-07-21 14:16:27 +0900

    feature: logging을 위해 Log4j 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,12 @@ dependencies {
     implementation("org.openjfx:javafx-fxml:$javafxVersion")
     implementation( "com.zaxxer:HikariCP:5.1.0")
     implementation ("com.oracle.database.jdbc:ojdbc11-production:21.18.0.0")
+
+    // Log4j
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1") // Use the latest stable version
+    runtimeOnly("org.apache.logging.log4j:log4j-core:2.23.1") // Use the latest stable version
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1") // Use the latest stable version
+
 }
 
 
