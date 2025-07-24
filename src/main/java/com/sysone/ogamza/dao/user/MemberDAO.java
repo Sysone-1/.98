142157374f61c0fcfe7782dbf11a54b65920477b
HeHelee <selly3109@naver.com>
2025-07-24 19:49:03 +0900

    refactor: SQL문 분리

--- src/main/java/com/sysone/ogamza/dao/user/MemberDAO.java
+++ src/main/java/com/sysone/ogamza/dao/user/MemberDAO.java
@@ -1,6 +1,7 @@
 package com.sysone.ogamza.dao.user;
 
 import com.sysone.ogamza.dto.user.MemberDetailDTO;
+import com.sysone.ogamza.sql.user.MemberSQL;
 import com.sysone.ogamza.utils.db.OracleConnector;
 
 import java.sql.*;
@@ -8,12 +9,12 @@ import java.sql.*;
 public class MemberDAO {
 
     public MemberDetailDTO findByEmail(String email) throws SQLException {
-        String query = "SELECT name, position, tel, password FROM employee WHERE email = ?";
+        String query = MemberSQL.SELECT_EMPLOYEE;
 
         try (Connection conn = OracleConnector.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setString(1, email);
-            try (ResultSet rs = stmt.executeQuery()) {
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setString(1, email);
+            try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
                     return new MemberDetailDTO(
                             rs.getString("name"),
@@ -28,13 +29,13 @@ public class MemberDAO {
     }
 
     public void updateByEmail(String email, String password, String tel) throws SQLException {
-        String sql = "UPDATE employee SET password = ?, tel = ? WHERE email = ?";
+        String sql = MemberSQL.UPDATE_EMPLOYEE;
         try (Connection conn = OracleConnector.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, password);
-            stmt.setString(2, tel);
-            stmt.setString(3, email);
-            stmt.executeUpdate();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, password);
+            pstmt.setString(2, tel);
+            pstmt.setString(3, email);
+            pstmt.executeUpdate();
         }
     }
 }
